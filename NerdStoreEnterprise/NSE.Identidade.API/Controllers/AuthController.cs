78b685ce6e364272dace3fd3dff1e3153fac3bf0
devleandrodias <leandrodbdias@gmail.com>
2020-11-15 13:12:20 -0300

    Criando api de autenticacao com asp net core identity

---
+++ NerdStoreEnterprise/NSE.Identidade.API/Controllers/AuthController.cs
@@ -0,0 +1,58 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using NSE.Identidade.API.Models;
+
+namespace NSE.Identidade.API.Controllers
+{
+    [Route("api/identidade")]
+    public class AuthController : Controller
+    {
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public AuthController(SignInManager<IdentityUser> signInManager, UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [HttpPost("nova-conta")]
+        public async Task<ActionResult> Registrar(UsuarioRegistro usuarioRegistro)
+        {
+            if (!ModelState.IsValid) return BadRequest();
+
+            IdentityUser user = new IdentityUser
+            {
+                UserName = usuarioRegistro.Email,
+                Email = usuarioRegistro.Email,
+                EmailConfirmed = true
+            };
+
+            IdentityResult result = await _userManager
+                .CreateAsync(user, usuarioRegistro.Senha);
+
+            if (result.Succeeded)
+            {
+                await _signInManager.SignInAsync(user, false);
+                return Ok();
+            }
+
+            return BadRequest();
+        }
+
+        [HttpPost("autenticar")]
+        public async Task<ActionResult> Login(UsuarioLogin usuarioRegistro)
+        {
+            if (!ModelState.IsValid) return BadRequest();
+
+            var result = await _signInManager
+                .PasswordSignInAsync(usuarioRegistro.Email, usuarioRegistro.Senha, false, true);
+
+            if (result.Succeeded) return Ok();
+
+            return BadRequest();
+        }
+    }
+}
