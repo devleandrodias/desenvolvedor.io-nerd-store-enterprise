01fcfc65ac177809e66ae6a3a861ce62fe22d087
devleandrodias <leandrodbdias@gmail.com>
2020-11-19 22:13:00 -0300

    configurando projeto mvc e classe startup

---
+++ NerdStoreEnterprise/NSE.WebApp.MVC/Configuration/WebAppConfig.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace NSE.WebApp.MVC.Configuration
+{
+    public static class WebAppConfig
+    {
+        public static void AddWebAppConfiguration(this IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+        }
+
+        public static void UseWepAppConfiguration(this IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseIdentityConfiguration();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
